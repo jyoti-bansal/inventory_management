32973c0bdc8c12586f8003c86092d43cc4ece907
Jyoti <yourgitemail@example.com>
2022-10-10 05:28:02 -0700

    Backend is ready

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const authRoute=require('./routes/auth.js');
 const userRoute=require('./routes/users');
 const userProduct=require('./routes/products');
 const userCart=require('./routes/cart');
+const config=require('config');
 
 const app=express();
 
-const port=3000;
-mongoose.connect("mongodb://localhost:27017/inventory",{
-    useNewUrlParser:true,
+//const isDevEnv = process.env.NODE_ENV === 'development';
+//const port=3000;
+const dbConfig=config.get('mongo.host');
+const portConfig=config.get('port');
+mongoose.connect(dbConfig);
+// mongoose.connect("mongodb://localhost:27017/inventory",{
+//     useNewUrlParser:true,
 
-})
-.then(()=>{
-    console.log("Connected!!!");
-})
-.catch((err)=>console.log(err));
+// })
+// .then(()=>{
+//     console.log("Connected!!!");
+// })
+// .catch((err)=>console.log(err));
 
 //Calling api routes from here......
 app.use(express.json());
@@ -24,6 +29,6 @@ app.use("/api/users",userRoute);
 app.use("/api/products",userProduct);
 app.use("/api/cart",userCart);
 
-app.listen(port,()=>{
+app.listen(portConfig,()=>{
     console.log("Listening....");
 })
